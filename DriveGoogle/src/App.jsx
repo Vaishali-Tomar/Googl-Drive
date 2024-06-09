c4298878b1c20c3cd7e4b957ba41b56a0029ae6e
Vaishali Tomar <vaishalitomar830@gmail.com>
2024-06-10 00:33:03 +0530

    more css added

--- DriveGoogle/src/App.jsx
+++ DriveGoogle/src/App.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'; 
-import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import { auth, provider } from "./firebase";
-import Data from './components/Data';
+import Data from "./components/Data";
 
 function App() {
   const [user, setUser] = useState(null);
 
   const signIn = () => {
-    auth.signInWithPopup(provider)
+    auth
+      .signInWithPopup(provider)
       .then(({ user }) => setUser(user))
-      .catch(err => alert(err));
+      .catch((err) => alert(err));
   };
 
   return (
@@ -25,18 +26,47 @@ function App() {
           </div>
         </>
       ) : (
-        <div className="bg-gray-200 p-5 w-96 mx-auto flex flex-col items-center mt-24">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png"
-            alt="gdrive"
-            className="w-24"
-          />
-          <button
-            onClick={signIn}
-            className="w-full bg-purple-800 p-3 text-white uppercase tracking-widest text-lg border-0 outline-0 rounded-md cursor-pointer mt-5"
-          >
-            Login to Google Drive
-          </button>
+        <div className="bg-gray-200 h-screen w-screen flex flex-col items-start pl-8 pt-4">
+          <div className="flex w-full justify-between items-center mb-10">
+            <div className="flex items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png"
+                alt="gdrive"
+                className="w-24 p-4"
+              />
+              <span className="text-3xl text-black font-semibold">Disk</span>
+            </div>
+            <button
+              onClick={signIn}
+              className="bg-purple-800 p-5 text-white uppercase tracking-widest text-lg border-0 outline-0 rounded-md cursor-pointer mr-8"
+            >
+              Login In
+            </button>
+          </div>
+          <div className="w-full  mt-22 pt-30 flex justify-evenly items-center flex-grow">
+            <div className="w-2/6 flex flex-col justify-center mr-8 ">
+              <h1 className="text-5xl mb-4 text-black font-semibold justify-center">
+                Easy and secure access to your content
+              </h1>
+              <p className="mb-4 text-center text-3xl text-gray-700">
+                Store, share, and collaborate on files and folders from your
+                mobile device, tablet, or computer.
+              </p>
+              <button
+                onClick={signIn}
+                className="w-full max-w-xs  bg-purple-800 p-3 text-white uppercase tracking-widest text-lg border-0 outline-0 rounded-md cursor-pointer mt-5"
+              >
+                Login In
+              </button>
+            </div>
+            <div className="w-2/5 flex justify-center">
+              <img
+                src="https://cdn.pixabay.com/photo/2016/04/14/07/53/girl-1328416_1280.jpg"
+                alt=""
+                className="max-h-96"
+              />
+            </div>
+          </div>
         </div>
       )}
     </>
--- DriveGoogle/src/components/Header.jsx
+++ DriveGoogle/src/components/Header.jsx
@@ -1,43 +1,38 @@
-import React from 'react'
+import React from "react";
 import { IoSearchOutline } from "react-icons/io5";
 import { MdFormatAlignCenter } from "react-icons/md";
 import { MdHelpOutline } from "react-icons/md";
 import { IoIosSettings } from "react-icons/io";
 import { IoMdApps } from "react-icons/io";
-import { CAvatar } from '@coreui/react'
 
 const Header = ({ photoURL }) => {
-    return (
-        <div className="grid grid-cols-3 items-center p-5 h-20 border-b border-gray-300">
-            <div className="flex items-center">
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/d/da/Google_Drive_logo.png"
-                    alt="Google Drive"
-                    className="w-10"
-                />
-                <span className="text-2xl ml-2 text-gray-600">Drive</span>
-            </div>
-            <div className="flex items-center w-full max-w-xl text-xl bg-gray-100 p-4 rounded-md">
-                <IoSearchOutline />
-                <input
-                    type="text"
-                    placeholder="Search in Drive"
-                    className="bg-transparent text-base pl-5 border-0 outline-none flex-1"
-                />
-                <MdFormatAlignCenter />
-            </div>
-            <div className="flex items-center justify-end text-xl">
-                <span className="flex gap-5 items-center ml-5 mr-10">
-                    <MdHelpOutline />
-                    <IoIosSettings />
-                </span>
-                <span className="flex gap-5 items-center">
-                    <IoMdApps />
-                    <CAvatar src={photoURL} />
-                </span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="grid grid-cols-[300px_1fr_300px] items-center p-5 h-15 border-b border-gray-300 ">
+      <div className="flex items-center">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/da/Google_Drive_logo.png"
+          alt="Google Drive"
+          className="w-10"
+        />
+        <span className="text-2xl ml-4 text-gray-600">Drive</span>
+      </div>
+      <div className="flex items-center bg-gray-100 p-3 rounded-lg mx-4 flex-grow">
+        <IoSearchOutline className="text-xl" />
+        <input
+          type="text"
+          placeholder="Search in Drive"
+          className="bg-transparent border-0 outline-none flex-1 ml-2 rounded-2xl"
+        />
+        <MdFormatAlignCenter className="text-xl" />
+      </div>
+      <div className="flex items-center justify-end space-x-4">
+        <MdHelpOutline className="text-xl" />
+        <IoIosSettings className="text-xl" />
+        <IoMdApps className="text-xl" />
+        <img src={photoURL} alt="User Avatar" className="ml-2 w-10 h-10 rounded-full" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
